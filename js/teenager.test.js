f0e75504786771ce16ea418ede89e23d19573523
Phil Metscher <philmetschermailt@gmx.de>
2022-09-14 16:18:06 +0200

    added test for teenager

--- js/teenager.test.js
+++ js/teenager.test.js
@@ -1,6 +1,16 @@
 import { checkTeenagerStatus } from "./teenager.js";
 
-test("returns correct teenager status", () => {
-  const teenagerStatus = checkTeenagerStatus(15);
-  expect(teenagerStatus).toBe(true);
-});
+const checkAges = [
+  [15, true],
+  [20, true],
+  [21, false],
+  [13, true],
+  [10, false],
+];
+
+test.each(checkAges)(
+  "returns correct teenager status",
+  (number, isTeenager) => {
+    expect(checkTeenagerStatus(number)).toBe(isTeenager);
+  }
+);
