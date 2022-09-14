8a02fbafc0e8023ed305a6554cec1f862233c3c7
Phil Metscher <philmetschermailt@gmx.de>
2022-09-14 17:09:40 +0200

    added some favicons

--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,14 @@
-function toggleBookmark(cards, id) {
+function toggleBookmark(cards, bookmarkCard) {
+  cards.filter((card) => {
+    if (card.id === bookmarkCard.id) {
+      if (card.bookmarked === true) {
+        return (card.bookmarked = false);
+      }
+      return (card.bookmarked = true);
+    } else {
+      return card;
+    }
+  });
   return cards;
 }
 
